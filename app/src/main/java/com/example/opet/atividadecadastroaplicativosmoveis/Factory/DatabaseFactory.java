b181ec7549465aea4aeaffe864fd3ce7f2f5dbc3
opet <opet@Academico.edu>
2018-04-18 11:00:37 -0300

    18/04/2018 11h00

--- app/src/main/java/com/example/opet/atividadecadastroaplicativosmoveis/Factory/DatabaseFactory.java
+++ app/src/main/java/com/example/opet/atividadecadastroaplicativosmoveis/Factory/DatabaseFactory.java
@@ -18,36 +18,19 @@ public class DatabaseFactory extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE "+ BancoUtil.TABELA_MARCA+"("
-                + BancoUtil.ID_MARCA + " integer primary key autoincrement,"
-                + BancoUtil.NOME_MARCA + " text,"
-                +")";
-        db.execSQL(sql);
-
-        sql = "CREATE TABLE "+ BancoUtil.TABELA_SETOR+"("
-                + BancoUtil.ID_SETOR + " integer primary key autoincrement,"
-                + BancoUtil.NOME_SETOR + " text,"
-                +")";
-        db.execSQL(sql);
-
-        sql = "CREATE TABLE "+ BancoUtil.TABELA_PRODUTO+"("
+        String sql = "CREATE TABLE "+ BancoUtil.TABELA_PRODUTO+"("
                 + BancoUtil.ID_PRODUTO+ " integer primary key autoincrement,"
                 + BancoUtil.NOME_PRODUTO + " text,"
                 + BancoUtil.DESCRICAO_PRODUTO + " text,"
                 + BancoUtil.VALIDADE_PRODUTO + " date,"
-                + BancoUtil.PRODUTO_MARCA + " integer,"
-                + " FOREIGN KEY (" + BancoUtil.PRODUTO_MARCA + ") REFERENCES " + BancoUtil.TABELA_MARCA + "(" + BancoUtil.ID_MARCA + ")"
-                +"),"
-                + " FOREIGN KEY (" + BancoUtil.PRODUTO_SETOR + ") REFERENCES " + BancoUtil.TABELA_SETOR + "(" + BancoUtil.ID_SETOR + ")"
-                +")";
+                + BancoUtil.SETOR_PRODUTO + " text,"
+                + BancoUtil.MARCA_PRODUTO + " text";
         db.execSQL(sql);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + BancoUtil.TABELA_MARCA);
-        db.execSQL("DROP TABLE IF EXISTS " + BancoUtil.TABELA_SETOR);
         db.execSQL("DROP TABLE IF EXISTS " + BancoUtil.TABELA_PRODUTO);
         onCreate(db);
     }
